632e7043e39f985594807fe26ffaef050dc22886
Stephen Cott <stephencott@gmail.com>
2018-04-25 21:53:09 -0700

    Made a change to the SUFFIX variable $SUFFIX -> $SUFFIX_BK
    Also $SUFFIX_BK no longer contains the '.'

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Check for current dotfiles in $HOME
 
 FILES="zshrc vimrc tmux.conf"
-SUFFIX=".bkup"
+SUFFIX_BK="bkup"
 BASEDIR=$(pwd)
 
 for FILE in $FILES
@@ -11,14 +11,14 @@ do
 	if [ -f $HOME/.$FILE ]
 	then
 		# Check if a .bkup file already exists and exit if it does
-		if [ -f $HOME/.$FILE.bkup ]
+		if [ -f $HOME/.$FILE.$SUFFIX_BK ]
 		then
 			echo "Found an existing .bkup file -- exiting"
 			exit 1
 		fi
 
 		# Rename existing files to .bkup
-		mv $HOME/.$FILE $HOME/.$FILE$SUFFIX
+		mv $HOME/.$FILE $HOME/.$FILE.$SUFFIX_BK
 	fi
 	# Create symlink for each file in $FILES
 	ln -s $BASEDIR/$FILE $HOME/.$FILE
